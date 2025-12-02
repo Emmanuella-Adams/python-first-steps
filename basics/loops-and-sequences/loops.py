d2dc66e518f378ecb8d64dd946deb6bc3c74a8c0
Emmanuella Adams <ellaadams206@gmail.com>
2025-12-02 11:22:32 +0100

    Refactor: Reintroduce loops, lists, and tuples examples with comprehensive explanations

---
+++ basics/loops-and-sequences/loops.py
@@ -0,0 +1,19 @@
+#we'll first go through the first loop
+
+color = ['red', 'blue', 'lavender', 'pink', 'purple', 'green', 'yello', 'brown']
+
+for char in color:
+    print(char)
+
+
+programming_languages = ['Rust', 'Java', 'Python', 'C++']
+
+for language in programming_languages:
+    print(language)
+
+categories = ['Fruit', 'Vegetable']
+foods = ['Apple', 'Carrot', 'Banana']
+
+for category in categories:
+    for food in foods:
+        print(category, food)
